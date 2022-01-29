7f5167ab7462f7dbd143db1fed76665f43638662
Skanda Aryal <skandaaryal2055@gmail.com>
2022-01-29 12:05:12 +0545

    Setup with twin macro

--- components/Dummy.tsx
+++ components/Dummy.tsx
@@ -8,8 +8,10 @@ const Dummy: FunctionComponent<DummyProps> = () => {
       <div className="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
         <div className="text-center space-y-2 sm:text-left">
           <div className="space-y-0.5">
-            <p className="text-lg text-black font-semibold">Erin Lindford</p>
-            <p className="text-slate-500 font-medium">Product Engineer</p>
+            <p className="text-lg text-black font-semibold">Skanda Aryal</p>
+            <p className="text-slate-500 font-medium">
+              Start editing and write some amazing code!
+            </p>
           </div>
           <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
             Message
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import "../styles/globals.css";
+import { GlobalStyles } from "twin.macro";
+
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <GlobalStyles />
+      <Component {...pageProps} />;
+    </div>
+  );
 }
 
 export default MyApp;
